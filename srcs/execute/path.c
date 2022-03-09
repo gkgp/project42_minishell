cfd4cdbc829b578eb742e71166228a97e348be55
Guillaume <92152391+GlaceCoding@users.noreply.github.com>
2022-03-09 21:52:55 +0100

    Fix segmentfault caché

--- srcs/execute/path.c
+++ srcs/execute/path.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   path.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: min-kang <minguk.gaang@gmail.com>          +#+  +:+       +#+        */
+/*   By: gphilipp <gphilipp@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/21 14:23:42 by min-kang          #+#    #+#             */
-/*   Updated: 2022/02/22 13:54:28 by min-kang         ###   ########.fr       */
+/*   Updated: 2022/03/09 21:50:03 by gphilipp         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char	*pathname_creator(char *s, char **paths)
+static char	*pathname_creator(char *s, char **paths)
 {
 	char	*cmd;
 	int		i;
@@ -20,7 +20,7 @@ char	*pathname_creator(char *s, char **paths)
 	if (access(s, F_OK) == 0)
 		return (s);
 	i = -1;
-	while (paths[++i])
+	while (paths && paths[++i])
 	{
 		cmd = ft_strjoin(paths[i], "/", 0);
 		cmd = ft_strjoin(cmd, s, 1);
@@ -33,7 +33,7 @@ char	*pathname_creator(char *s, char **paths)
 	return (NULL);
 }
 
-char	**possible_path(char **envp)
+static char	**possible_path(char **envp)
 {
 	int		i;
 	char	**result;
