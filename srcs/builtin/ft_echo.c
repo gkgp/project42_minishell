3e1abc8c19cd932285a6cdda38260227d59fe2d9
Guillaume <92152391+GlaceCoding@users.noreply.github.com>
2022-03-14 20:21:23 +0100

    support 'echo -n- -nnnn- -n ok'

--- srcs/builtin/ft_echo.c
+++ srcs/builtin/ft_echo.c
@@ -6,11 +6,26 @@
 /*   By: gphilipp <gphilipp@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 08:37:45 by gkgpteam          #+#    #+#             */
-/*   Updated: 2022/02/23 15:35:59 by gphilipp         ###   ########.fr       */
+/*   Updated: 2022/03/14 20:19:41 by gphilipp         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static int	is_n_argument(char *str)
+{
+	if (*str++ != '-')
+		return (0);
+	while (*str && *str == 'n')
+	{
+		if (*(str + 1) == 0)
+			return (1);
+		if (*(str + 1) == 0)
+			return (0);
+		str++;
+	}
+	return (0);
+}
 
 int	ft_echo(char **args)
 {
@@ -20,12 +35,17 @@ int	ft_echo(char **args)
 	if (!args || !*args)
 		return (0);
 	i = -1;
-	flag_n = (ft_strcmp("-n", args[i + 1]) == 0 && ++i >= 0);
+	flag_n = 0;
+	while (args[i + 1] && is_n_argument(args[i + 1]))
+	{
+		flag_n = 1;
+		i++;
+	}
 	while (args[++i])
 	{
-		if (i - flag_n > 0)
-			ft_putstr_fd(" ", 1);
 		ft_putstr_fd(args[i], 1);
+		if (args[i + 1])
+			ft_putstr_fd(" ", 1);
 	}
 	if (!flag_n)
 		ft_putstr_fd("\n", 1);
